9f2e82eb3c1f6af929896c01b65c6fa494de29a3
sisprojeti <sisprojeti@gmail.com>
2019-11-29 15:33:23 -0300

    update anexos

--- modulos/anexos/adicionar.php
+++ modulos/anexos/adicionar.php
@@ -1,6 +1,24 @@
-<div class="container">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <style>
+       .error{
+             color:red
+       }
+</style>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+  <link rel="stylesheet" href="">
+</head>
+<body>
+  <div class="container">
   <div class="row it">
-    <div class="col-sm-offset-1 col-sm-10" id="one">
+    <div class="col-sm-offset-1 col-lg-12" id="one">
       <p>
         Área destinada para envio de documentos da Av1, Av2 e Av3.
       </p><br>
@@ -11,6 +29,7 @@
         <!--form-group-->
       </div>
       <!--row-->
+      <form role="form" id="form_anexos" action="#" method="POST">
       <div id="uploader">
         <div class="row uploadDoc">
           <div class="col-sm-3">
@@ -19,13 +38,13 @@
             <div class="fileUpload btn btn-orange">
               <img src="https://image.flaticon.com/icons/svg/136/136549.svg" class="icon">
               <span class="upl" id="upload">Selecionar arquivo</span>
-              <input type="file" class="upload up" id="up" onchange="readURL(this);" />
+              <input type="file" class="upload up" name="up" id="up" onchange="readURL(this);" required  />
             </div>
             <!--classe btn-orange -->
           </div>
           <!--classe col-3 -->
           <div class="col-sm-8 ">
-            <input type="text" class="form-control" name="" placeholder="Descrição...">
+            <input type="text" class="form-control" name="descricao" id="descricao" placeholder="Descrição..." required autofocus>
           </div>
           <!--classe col-8-->
           <div class="col-sm-1"><a class="btn-check"><i class="fa fa-times"></i></a></div>
@@ -36,10 +55,37 @@
       <!--uploader-->
       <div class="text-center">
         <a href="#" class="btn btn-outline-dark"><i class="fa fa-plus"></i> Adicionar novo</a>
-        <a href="#" class="btn btn-outline-dark"><i class="fa fa-paper-plane"></i> Enviar</a>
+        <button type="submit" class="btn btn-outline-primary"><i class="fa fa-paper-plane"></i> Enviar</button>
       </div>
     </div>
     <!--one-->
   </div>
   <!-- row -->
 </div>
+</form>
+<script>
+            $("#form_anexos").validate({
+       rules : {
+              up:{
+                    required:true
+             },
+             descricao:{
+                    
+                    required:true,
+             },                                  
+       },
+       messages:{
+              up:{
+                    required:"Por favor, carregue um documento válido."
+             },
+              descricao:{
+                    required:"Por favor, insira uma descricao"
+             },  
+       }
+
+});
+
+
+       </script>
+</body>
+</html>
