745b45f6d58e248711b8706409004c0680e66034
diegobrbosa <diego_barbosa_souza@hotmail.com>
2020-03-11 13:57:17 -0300

    criando class de refAlunoTarefa

---
+++ classes/class.refAlunoTarefa.php
@@ -0,0 +1,20 @@
+<?php
+
+    class RefAlunoTarefa
+    {
+      public $id_ref_aluno_tarefa;
+      public $fk_tarefa;
+      public $fk_ref_aluno_projeti;
+
+      public function adicionar()
+      {
+        $sql = "INSERT INTO ref_aluno_tarefa(fk_tarefa,fk_ref_aluno_projeti) values (:fk_tarefa,:fk_ref_aluno_projeti)";
+        $conexao = DB::conexao();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(':fk_tarefa',$this->fk_tarefa);
+        $stmt->bindParam(':fk_ref_aluno_projeti',$this->fk_ref_aluno_projeti);
+        $stmt->execute();
+      }
+    }
+
+?>
