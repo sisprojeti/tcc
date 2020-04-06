172274a7605fabdbe7b24b868664ba728414f7a2
Gabi <gabrielaexposto1@gmail.com>
2020-04-06 20:25:20 -0300

    atualizar tela de update de tarefas

--- modulos/tarefa/home.php
+++ modulos/tarefa/home.php
@@ -281,15 +281,69 @@ if(isset($_POST["button"]) && ($_POST["button"] === "Detalhes")){
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="container col-lg-12 navbar-white">
-         <section class="content navbar-light navbar-white">
-           <div class="container-fluid navbar-white" id="conteudo_tarefa">
+      <div class="modal-body">
+        <form action="#" method="post">
+          <div class="form-group">
+            <label class="col-form-label">Título:</label>
+            <input type="text" class="form-control" name="titulo" id="titulo">
+          </div>
+          <div class="form-group">
+            <label class="col-form-label">Descrição:</label>
+            <textarea class="form-control" id="descricao" name="descricao"></textarea>
+          </div>
+           <div class="row">
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label>Data de Ínicio:</label>
+                        <input type="date" class="form-control" name="data_inicio" id="data_inicio" >
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Data de Término:</label>
+                        <input type="date" class="form-control" name="data_fim" id="data_fim">
+                      </div>
+                    </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Data de Entrega:</label>
+                        <input type="date" class="form-control" name="data_conclusao" id="data_entrega">
+                      </div>
+                    </div>
+                     <!-- select -->
 
-           </div><!-- /.container-fluid -->
-         </section>
-       </div>
+                     <div class="col-sm-6">
+                       <label>Status Tarefa</label>
+                       <select class="form-control" name="fk_status_tarefa" required autofocus>
+                        <option value="">Selecione o status</option>
+                         <?php if(isset($listarStatus)):?>
+                           <?php foreach ($listarStatus as $status):?>
+                             <?php //if($aluno->getSituacaoAluno()):?>
+                             <option value="<?php echo $status->getIdStatusTarefa();?>"><?php echo $status->getNomeStatusTarefa();?></option>
+                           <?php endforeach;?>
+                         <?php endif;?>
+                       </select>
+                     </div>
+              </div>
+
+              <div class="col-sm-6">
+                <label>Responsável</label>
+                <select class="form-control" name="fk_ref_aluno_projeti" required autofocus>
+                 <option value="">Selecione o Responsável</option>
+                  <?php if(isset($listarAlunosProjeti)):?>
+                    <?php foreach ($listarAlunosProjeti as $alunosProjeti):?>
+                      <?php //if($aluno->getSituacaoAluno()):?>
+                      <option value="<?php echo $alunosProjeti->getIdRefAlunoProjeti();?>"><?php echo $alunosProjeti->getNomeAlunoProjeti();?></option>
+                    <?php endforeach;?>
+                  <?php endif;?>
+                </select>
+              </div>
+      </div>
       <div class="modal-footer">
-        <button type="button"  class="btn btn-secondary fechar-detalhe" data-dismiss="modal">Fechar</button>
+        <input type="submit" name="button" value="Salvar" class="btn btn-primary" >
       </div>
     </div>
   </div>
