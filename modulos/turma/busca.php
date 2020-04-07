ce6960c45b97323df6a88498b8d665b368b51daf
Gabi <gabrielaexposto1@gmail.com>
2020-04-06 21:10:31 -0300

    {ALTERADO botão de sair

---
+++ modulos/turma/busca.php
@@ -0,0 +1,8 @@
+<div class="input-group input-group-sm">
+        <input style="width: 300px;"class="form-control form-control-navbar" type="search" placeholder="Pesquisar" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
