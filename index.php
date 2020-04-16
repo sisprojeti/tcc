5644240c2273f2891ef5dcdf5577de5ea76ae2fe
Gabi <gabrielaexposto1@gmail.com>
2020-04-16 11:16:30 -0300

    {ATUALIZADA} organização

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
     session_start();
-    if(!isset($_SESSION['cpf']) || !isset($_SESSION['fk_usuario'])){
-      echo "<script>location.href='login.php?msg=erro'</script>";
-    }
 
     if(isset($_POST['botao']) && $_POST["botao"] == "Sair"){
       session_destroy();
       header('Location:login.php');
     }
     //print_r($_SESSION);
+    if(!isset($_SESSION['cpf']) || !isset($_SESSION['fk_usuario'])){
+      echo "<script>location.href='login.php?msg=erro'</script>";
+    } else {
 
 ?>
 <!DOCTYPE html>
@@ -18,104 +18,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SISP - Conteudo Dinamico</title>
   <link rel="shortcut icon" href="img/logos.png">
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="pluginOs/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <!-- CSS customizados para essa página de CADASTRO -->
-  <link rel="stylesheet" type="text/css" href="css/anexos.css">
-  <!-- Logo do Sistema -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"> </script>
-    <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/select2/dist/css/select2.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <script>
-			function editar(id, txt_tarefa) {
-
-				//criar um form de edição
-				let form = document.createElement('form')
-				form.action = 'index.php?modulo=tarefa&acao=tarefa_atualizar'
-				form.method = 'post'
-				form.className = 'row'
-
-				//criar um input para entrada do texto
-				let inputTarefa = document.createElement('input')
-				inputTarefa.type = 'text'
-				inputTarefa.name = 'tarefa'
-				inputTarefa.className = 'col-9 form-control'
-				inputTarefa.value = txt_tarefa
-
-				//criar um input hidden para guardar o id da tarefa
-				let inputId = document.createElement('input')
-				inputId.type = 'hidden'
-				inputId.name = 'id'
-				inputId.value = id
-
-				//criar um button para envio do form
-				let button = document.createElement('button')
-				button.type = 'submit'
-				button.className = 'col-3 btn btn-info'
-				button.innerHTML = 'Atualizar'
-
-				//incluir inputTarefa no form
-				form.appendChild(inputTarefa)
-
-				//incluir inputId no form
-				form.appendChild(inputId)
-
-				//incluir button no form
-				form.appendChild(button)
-
-				//teste
-				//console.log(form)
-
-				//selecionar a div tarefa
-				let tarefa = document.getElementById('tarefa_'+id)
-
-				//limpar o texto da tarefa para inclusão do form
-				tarefa.innerHTML = ''
-
-				//incluir form na página
-				tarefa.insertBefore(form, tarefa[0])
-
-			}
-
-			function remover(id) {
-				location.href = 'todas_tarefas.php?acao=remover&id='+id;
-			}
-
-			function marcarRealizada(id) {
-				location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
-			}
-		</script>
-
-    		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/formularios.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+  <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
 </head>
 <!--<body class="hold-transition sidebar-mini layout-fixed"> -->
 <body class="sidebar-mini skin-yellow-light" style="height: auto; min-height: 100%;">
@@ -533,12 +441,9 @@
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+</body>
+<footer>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
 <script type="text/javascript" src="js/anexos.js"></script>
 <script>
   $.widget.bridge('uibutton', $.ui.button)
@@ -570,6 +475,102 @@
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script><!-- PELO CDN -->
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="pluginOs/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS customizados para essa página de CADASTRO -->
+  <link rel="stylesheet" type="text/css" href="css/anexos.css">
+  <!-- Logo do Sistema -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/formularios.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"> </script>
+  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/select2/dist/css/select2.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <script>
+      function editar(id, txt_tarefa) {
 
-</body>
+        //criar um form de edição
+        let form = document.createElement('form')
+        form.action = 'index.php?modulo=tarefa&acao=tarefa_atualizar'
+        form.method = 'post'
+        form.className = 'row'
+
+        //criar um input para entrada do texto
+        let inputTarefa = document.createElement('input')
+        inputTarefa.type = 'text'
+        inputTarefa.name = 'tarefa'
+        inputTarefa.className = 'col-9 form-control'
+        inputTarefa.value = txt_tarefa
+
+        //criar um input hidden para guardar o id da tarefa
+        let inputId = document.createElement('input')
+        inputId.type = 'hidden'
+        inputId.name = 'id'
+        inputId.value = id
+
+        //criar um button para envio do form
+        let button = document.createElement('button')
+        button.type = 'submit'
+        button.className = 'col-3 btn btn-info'
+        button.innerHTML = 'Atualizar'
+
+        //incluir inputTarefa no form
+        form.appendChild(inputTarefa)
+
+        //incluir inputId no form
+        form.appendChild(inputId)
+
+        //incluir button no form
+        form.appendChild(button)
+
+        //teste
+        //console.log(form)
+
+        //selecionar a div tarefa
+        let tarefa = document.getElementById('tarefa_'+id)
+
+        //limpar o texto da tarefa para inclusão do form
+        tarefa.innerHTML = ''
+
+        //incluir form na página
+        tarefa.insertBefore(form, tarefa[0])
+
+      }
+
+      function remover(id) {
+        location.href = 'todas_tarefas.php?acao=remover&id='+id;
+      }
+
+      function marcarRealizada(id) {
+        location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+      }
+    </script>
+</footer>
 </html>
+<?php } ?>
