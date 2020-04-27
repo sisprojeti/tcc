44bd67ae71e4c90f2b19b170e275bab6ce068b4a
Gabi <gabrielaexposto1@gmail.com>
2020-04-27 20:00:38 -0300

    {ATUALIZADA} linha de consulta

--- banco/consulta.sql
+++ banco/consulta.sql
@@ -17,4 +17,23 @@ SELECT ref_aluno_projeti.fk_projeti as fk_projeti,aluno.id_aluno as id_aluno,alu
 as id_pessoa, pessoa.nome as nome_aluno FROM aluno
 join pessoa on aluno.fk_pessoa = pessoa.id_pessoa
 join ref_aluno_projeti on aluno.id_aluno = ref_aluno_projeti.fk_aluno
-where aluno.fk_pessoa = '1' and ref_aluno_projeti.fk_projeti in (SELECT fk_projeti FROM ref_aluno_projeti where fk_aluno = aluno.id_aluno)
+where aluno.fk_pessoa = '2' and ref_aluno_projeti.fk_projeti in 
+(SELECT fk_projeti FROM ref_aluno_projeti)
+
+/* ID PESSOA */
+SELECT pessoa.nome as nome_aluno, aluno.id_aluno as id_aluno, ref_aluno_projeti.fk_projeti as id_projeti
+FROM pessoa 
+JOIN aluno on pessoa.id_pessoa = aluno.fk_pessoa
+JOIN ref_aluno_projeti on aluno.id_aluno = ref_aluno_projeti.fk_aluno
+WHERE pessoa.id_pessoa = 2;
+
+/* ID PROJETI */
+SELECT pessoa.nome as nome_aluno, aluno.id_aluno as id_aluno, ref_aluno_projeti.fk_projeti as id_projeti
+FROM pessoa 
+JOIN aluno on pessoa.id_pessoa = aluno.fk_pessoa
+JOIN ref_aluno_projeti on aluno.id_aluno = ref_aluno_projeti.fk_aluno
+WHERE ref_aluno_projeti.fk_projeti = 1;
+
+
+SELECT pessoa.nome as nome_aluno, aluno.id_aluno as id_aluno, 
+FROM ref_aluno_projeti JOIN alu
