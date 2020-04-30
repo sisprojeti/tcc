2c0782472c5916d927d4cf051b71f41d7e6c7091
sisprojeti <sisprojeti@gmail.com>
2020-04-30 14:35:06 -0300

    validações
    validações campo "nova_tarefa"

--- modulos/tarefa/adicionar.php
+++ modulos/tarefa/adicionar.php
@@ -58,7 +58,47 @@
   }
 ?>
 
-  <div class="modal-dialog modal-lg">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title></title>
+  <style>
+       .error{
+             color:red
+       }
+</style>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+      <script>
+        function ValidarDatas() {
+              var data1 = $("#data_inicio").val();
+              var data2 = $("#data_fim").val();
+              
+              var dataInicial = ConverteParaData(data1);
+              var dataFinal   = ConverteParaData(data2);
+              
+              
+              if (dataInicial > dataFinal) {
+                  console.log("Data inválida!");
+              }else{
+                  console.log("Data válida!");
+              }
+          }
+
+          function ConverteParaData(data){
+            var dataArray = data.split('/');
+            var novaData = new Date(dataArray[2], dataArray[1], dataArray[0]);
+            
+            return novaData;
+          }
+      </script>
+</head>
+<body>
+    <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Nova Tarefa</h5>
@@ -67,27 +107,27 @@
         </button>
       </div>
       <div class="modal-body">
-        <form action="#" id="nova_tarefa" method="post">
+        <form action="#" id="tarefa" method="post">
           <div class="form-group">
             <label class="col-form-label">Título:</label>
-            <input type="text" class="form-control" name="titulo" id="titulo" required>
+            <input type="text" class="form-control" name="titulo" required>
           </div>
           <div class="form-group">
             <label class="col-form-label">Descrição:</label>
-            <textarea class="form-control" id="descricao" name="descricao" required></textarea>
+            <textarea class="form-control"  name="descricao" required></textarea>
           </div>
            <div class="row">
                     <div class="col-sm-6">
                       <!-- text input -->
                       <div class="form-group">
                         <label>Data de Ínicio:</label>
-                        <input type="date" class="form-control" name="data_inicio" id="data_inicio" required>
+                        <input type="date" class="form-control" name="data_inicio" required>
                       </div>
                     </div>
                     <div class="col-sm-6">
                       <div class="form-group">
                         <label>Data de Término:</label>
-                        <input type="date" class="form-control" name="data_fim" id="data_fim" required>
+                        <input type="date" class="form-control" name="data_fim" required>
                       </div>
                     </div>
             </div>
@@ -95,7 +135,7 @@
                 <div class="col-sm-6">
                       <div class="form-group">
                         <label>Data de Entrega:</label>
-                        <input type="date" class="form-control" name="data_entrega" id="data_entrega" required>
+                        <input type="date" class="form-control" name="data_entrega" required>
                       </div>
                     </div>
                      <!-- select -->
@@ -130,17 +170,14 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-        <input type="submit" name="button" value="Salvar" class="btn btn-primary" >
+        <input type="submit" name="button" value="Salvar" class="btn btn-primary" onclick="ValidarDatas()">
       </div>
       </form>
     </div>
   </div>
 </div>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
-
       <script>
-            $("#nova_tarefa").validate({
+            $("#tarefa").validate({
        rules : {
              titulo:{
                     required:true,
@@ -161,27 +198,41 @@
                     required:true,
 
              },
+             fk_status_tarefa:{
+                    required:true,
+             },
+             fk_aluno:{
+                    required:true,
+             }
        },
        messages:{
-            projeti:{
-                    required:"Por favor, insira o tema do projeti",
+            titulo:{
+                    required:"Por favor, insira o título da tarefa",
                     minlength:"No mínimo 6 letras",
              },
              descricao:{
                     required:"Por favor, informe a descricao",
                     minlength:"No mínimo 6 letras",
              },
-             aluno_um:{
-                    required:"Por favor, selecione um aluno",
+             data_inicio:{
+                    required:"Por favor, informe a data de inicio",
 
              },
-             aluno_dois:{
-                    required:"Por favor, selecione um aluno",
+              data_fim:{
+                    required:"Por favor, informe a data de fim",
              },
-             aluno_tres:{
-                    required:"Por favor, selecione um aluno",
+             data_entrega:{
+                    required:"Por favor, informe a data de entrega",
              },
+             fk_status_tarefa:{
+                    required:" Informe o status da tarefa!",
+             },
+             fk_aluno:{
+                    required:"Selecione um Responsável",
+             }
        }
 });
 
 </script>
+</body>
+</html>
