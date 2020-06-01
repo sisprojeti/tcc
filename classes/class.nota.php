c68d6fb2c45bbc5c3d72842e21c70944e9092d75
diegobrbosa <diego_barbosa_souza@hotmail.com>
2020-06-01 18:53:14 -0300

    atualização na class de notas

--- classes/class.nota.php
+++ classes/class.nota.php
@@ -16,9 +16,9 @@
         $stmt = $conexao->prepare($sql);
         $stmt->bindParam(':valor',$this->valor);
         $stmt->bindParam(':data_modificao',$this->data_modificao);
-        $stmt->bindParam(':fk_criterio',$this->fk_projeti);
-        $stmt->bindParam(':fk_aluno',$this->fk_professor);
-        $stmt->bindParam(':fk_professor',$this->fk_projeti);
+        $stmt->bindParam(':fk_criterio',$this->fk_criterio);
+        $stmt->bindParam(':fk_aluno',$this->fk_aluno);
+        $stmt->bindParam(':fk_professor',$this->fk_professor);
         $stmt->execute();
       }
 
@@ -26,20 +26,40 @@
         $this->valor = $valor;
       }
 
+      public function getValor(){
+        return $this->valor;
+      }
+
       public function setDataModificacao($data_modificacao){
         $this->data_modificacao = $data_modificacao;
+      }
+
+      public function getDataModificacao(){
+        return $this->data_modificao;
       }
 
       public function setFkCriterio($fk_criterio){
         $this->fk_criterio = $fk_criterio;
       }
 
+      public function getFkCriterio(){
+        return $this->fk_criterio;
+      }
+
       public function setFkAluno($fk_aluno){
         $this->fk_aluno = $fk_aluno;
       }
 
+      public function getFkAluno(){
+        return $this->fk_aluno;
+      }
+
       public function setFkProfessor($fk_professor){
         $this->fk_professor = $fk_professor;
+      }
+
+      public function getFkProfessor(){
+        return $this->fk_professor;
       }
 
 
